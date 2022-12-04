ad414530e6e80a3f153ebd9e8936c9cbb37c4c1f
paigexc70 <catherinep1090@gmail.com>
2022-12-04 16:49:30 -0600

    updated css

--- project1/urls.py
+++ project1/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from project1.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appone.urls')),
+    path('', home),
 ]
