6520fabe7948e50cb908f3bf22aee6d4143faf5b
paigexc70 <catherinep1090@gmail.com>
2022-12-14 01:45:56 -0600

    login and logout

--- project1/urls.py
+++ project1/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from project1.views import home
 from register import views as reg_views
@@ -21,6 +22,8 @@ from register import views as reg_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', reg_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='templates/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='templates/logout.html'), name='logout'),
     path('', include('appone.urls')),
     path('', home),
 ]
--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}!')
-            return redirect('home')
+            return redirect('login')
      else:
         form = UserRegisterForm()
      return render (request, 'registration.html', {'form': form})
