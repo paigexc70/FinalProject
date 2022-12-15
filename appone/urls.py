82aa7421a534bfe5f71abf3e22bcf214f667dd1e
paigexc70 <catherinep1090@gmail.com>
2022-12-14 23:28:57 -0600

    fixed

--- appone/urls.py
+++ appone/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import MainView, DetailView
+from .views import MainView, DetailView, AddPostView
 
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     #path('fpost/', views.fpost, name="fpost"),
     path('', MainView.as_view(), name="main"),
     path('article/<int:pk>', DetailView.as_view(), name='fpost'),
+    path('addpost/', AddPostView.as_view(), name='add_post'),
  
   
 ]
--- appone/views.py
+++ appone/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 #def index(request):
@@ -16,3 +16,8 @@ class MainView(ListView):
 class DetailView(DetailView):
     model = Post
     template_name = 'fpost.html'
+
+class AddPostView(CreateView):
+   model = Post
+   template_name = 'addpost.html'
+   fields = '__all__'
